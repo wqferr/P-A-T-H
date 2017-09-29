69a4e43db97edd09ebed61cfa625a16d53887d47
wqferr <wqferr@gmail.com>
2017-09-29 12:12:08 +0000

    Start documenting

--- include/misc/compare.h
+++ include/misc/compare.h
@@ -1,6 +1,16 @@
 #ifndef FUNC_H
 #define FUNC_H 1
 
-typedef int (*cmp_f)(const void *, const void *);
+/*
+ * Comparison function.
+ * Must return 0 if and only if the elements the references point to
+ * are equivalent.
+ * If the elements have a notion of order, a positive return value
+ * implies
+ * 		*(ref1) > *(ref2)
+ * Likewise, a negative return value implies
+ * 		*(ref1) < *(ref2)
+ */
+typedef int (*cmp_f)(const void *ref1, const void *ref2);
 
 #endif
--- include/misc/hash.h
+++ include/misc/hash.h
@@ -3,6 +3,15 @@
 
 #include <stdint.h>
 
+/*
+ * Hash function.
+ * Must be consistent with a comparison function (see compare.h).
+ * That is, given two elements, e1 and e2, and their
+ * associated comparison function cmp
+ * 		cmp(e1, e2) == 0  =>  hash(e1) == hash(e2)
+ * Note that the relation is one way: if the hashes for two elements
+ * are equal, that does NOT necessarily mean they are equivalent.
+ */
 typedef int32_t (*hash_f)(const void *);
 
 #endif
